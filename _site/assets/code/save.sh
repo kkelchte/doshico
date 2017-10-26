6f69d44071c37d6a31fb0819e32881aca05d7c53
klaas kelchtermans <klaas.kelchtermans@esat.kuleuven.be>
2017-10-27 01:21:17 +0200

    update without _site

--- _site/assets/code/save.sh
+++
@@ -1,6 +0,0 @@
-# Run the running container as your image
-# so the updates on system level in the container are not lost
-# pick a version number to keep track of your changes
-
-NUM=0.0
-sudo docker commit doshico_container doshico_image:$NUM
